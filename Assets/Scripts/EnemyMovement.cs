d7083042681e99102374c417fc779b3dc6fa440b
leducdat-tpose <leducdat0907@gmail.com>
2024-08-28 16:20:18 +0700

    Add simple enemy move around

---
+++ Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[RequireComponent(typeof(Rigidbody2D))]
+public class EnemyMovement : MonoBehaviour
+{
+    [Header("References")]
+    [SerializeField]
+    Rigidbody2D rigid;
+
+    [Header("Attributes")]
+    [SerializeField]
+    float moveSpeed = 10f;
+
+    int pathIndex = 0;
+    Transform target;
+
+    private void Reset()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+        rigid.bodyType = RigidbodyType2D.Kinematic;
+    }
+
+    void Start()
+    {
+        transform.position = LevelManager.instance.startPoint.position;
+        target = LevelManager.instance.paths[pathIndex];
+    }
+
+    void Update()
+    {
+        if(Vector2.Distance(target.position, transform.position) < 0.1f)
+        {
+            pathIndex++;
+            if(pathIndex == LevelManager.instance.paths.Length)
+            {
+                this.gameObject.SetActive(false);
+                return;
+            }
+            target = LevelManager.instance.paths[pathIndex];
+        }
+    }
+    private void FixedUpdate()
+    {
+        Vector2 direction = (target.position - transform.position).normalized;
+        rigid.velocity = direction * moveSpeed;
+    }
+}
