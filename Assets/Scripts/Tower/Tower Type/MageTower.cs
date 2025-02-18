5ad20ea318ba5eb5eb232817a93a12c361e69614
leducdat-tpose <leducdat0907@gmail.com>
2025-02-18 22:58:18 +0700

    Add mage tower, artillery tower's animation

--- Assets/Scripts/Tower/Tower Type/MageTower.cs
+++ Assets/Scripts/Tower/Tower Type/MageTower.cs
@@ -7,12 +7,19 @@ public class MageTower : Tower
 {
     [SerializeField]
     private Animator _animator;
+    [SerializeField, Range(1,3)]
+    private int _level = 1;
 
     protected override void Start()
     {
         base.Start();
         _animator = GetComponent<Animator>();
         HaveAnimation = true;
+        for(int i = 0; i < _animator.layerCount; i++)
+        {
+            if(i == (_level - 1)) _animator.SetLayerWeight(i, 1);
+            else _animator.SetLayerWeight(i, 0);
+        }
     }
 
     protected override void Update()
@@ -26,7 +33,7 @@ public class MageTower : Tower
         var newAnimation = Idle;
         if(InProgress) newAnimation = StartingProgress;
         if (newAnimation == _currentAnimation) return;
-        _animator.CrossFade(newAnimation, 0.2f);
+        _animator.CrossFade(newAnimation, 0.2f, _level - 1);
         _currentAnimation = newAnimation;
     }
 }
